6554ba323a7399d515a59c4be16261535821c672
Diego Pérez Hernández <59278575+DiegopH-m@users.noreply.github.com>
2024-01-19 22:40:07 +0000

    Fix local bug

--- Calculator.php
+++ Calculator.php
@@ -4,16 +4,16 @@ class Calculator {
     public function calculate($num1, $num2, $operator) {
         switch ($operator) {
             case '+':
-                return $num1 - $num2;
-            case '-':
                 return $num1 + $num2;
+            case '-':
+                return $num1 - $num2;
             case '*':
                 return $num1 * $num2;
             case '/':
                 if ($num2 != 0) {
                     return $num1 / $num2;
                 } else {
-                    return "Cannot divide by zero";
+                    return "Cannot divide by .";
                 }
             default:
                 return "Invalid operator";
