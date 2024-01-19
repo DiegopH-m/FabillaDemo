88ab714ec6ad663feac069a444e5cc4e223980c6
Diego Pérez Hernández <59278575+DiegopH-m@users.noreply.github.com>
2024-01-19 19:58:57 +0000

    Broken PR

--- Calculator.php
+++ Calculator.php
@@ -4,9 +4,9 @@ class Calculator {
     public function calculate($num1, $num2, $operator) {
         switch ($operator) {
             case '+':
-                return $num1 + $num2;
-            case '-':
                 return $num1 - $num2;
+            case '-':
+                return $num1 + $num2;
             case '*':
                 return $num1 * $num2;
             case '/':
